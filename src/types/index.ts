f86e79cbf20936bb8a0a917cbe0b4cfdd5e24321
Mikhail Barycheuski <m.barycheuski@godeltech.com>
2025-03-09 22:57:36 +0100

    Add favorites movie filter form

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,5 @@
 export type { default as SearchFormInput } from "./SearchFormInput";
 export type { default as FavoriteMovie } from "./FavoriteMovie";
 export type { default as Movie } from "./Movie";
+export type { default as FavoriteMovieFilterFormInput } from "./FavoriteMovieFilterFormInput";
+export { default as FavoriteMovieStatus } from "./FavoriteMovieStatus";
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,4 @@
 export * from "./numberUtils";
 export * from "./textUtils";
 export * from "./searchParamsUtils";
+export * from "./movieUtils";
